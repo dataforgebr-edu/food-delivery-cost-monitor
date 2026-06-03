5c52462dbd167c848294b2961a98b08a8aaf0cc1
RodrigoSilva1989 <dataforge.br@gmail.com>
2026-06-03 17:24:12 -0300

    build: criação da função de upload files to s3 and register partition on glue

--- pipeline/ingestion/upload_to_s3.py
+++ pipeline/ingestion/upload_to_s3.py
@@ -10,7 +10,10 @@ from pipeline_config import (
     API_REGION,
     BUCKET_NAME,
     RAW_LAYER_PREFIX,
-    DEFAULT_LOCAL_DIR
+    DEFAULT_LOCAL_DIR,
+    ATHENA_DATABASE,
+    TABLE_JOBS_LOGS,
+    RAW_LAYER_LOCATION
 
 )
 
@@ -27,7 +30,7 @@ def list_local_dates(local_dir: Path) -> List[str]:
         logger.error(f"Diretório {local_dir} não encontrado")
         raise
 
-def list_uploaded_dates(client: Any, bucket: str, prefix: str) -> List[str]:
+def s3_list_uploaded_dates(client: Any, bucket: str, prefix: str) -> List[str]:
     try:
         response = client.list_objects_v2(
             Bucket=bucket,
@@ -48,13 +51,60 @@ def list_uploaded_dates(client: Any, bucket: str, prefix: str) -> List[str]:
         else:
             raise
         
-def check_dates_to_upload(client: Any, bucket: str, prefix: str, local_dir: Path) -> List[str]:
+def s3_check_dates_to_upload(client: Any, bucket: str, prefix: str, local_dir: Path) -> List[str]:
     try:
         local_dates = list_local_dates(local_dir)
-        uploaded_dates = list_uploaded_dates(client, bucket, prefix)
+        uploaded_dates = s3_list_uploaded_dates(client, bucket, prefix)
         return [date for date in local_dates if date not in uploaded_dates]
     except Exception as e:
         logger.error(f"Erro ao verificar datas para upload: {e}")
+        raise
+
+def s3_upload_file(client: Any, bucket: str, local_dir: str, s3_prefix: str, file_date: str) -> bool:
+    local_path = f"{local_dir}/date={file_date}/data.parquet"
+    s3_path = f"{s3_prefix}date={file_date}/data.parquet"
+    try:
+        client.upload_file(
+            Filename=local_path,
+            Bucket=bucket,
+            Key=s3_path
+        )
+        logger.info(f"Arquivo {local_path} enviado para s3://{bucket}/{s3_path}")
+        return True
+    except ClientError as e:
+        logger.error(f"Erro ao enviar arquivo {local_path} para s3: {e}")
+        raise
+
+def glue_register_partition(database_name: str, table_name: str, file_date: str, s3_location: str) -> bool:
+    try:
+        final_location = f"{s3_location}date={file_date}/"
+        glue_client = boto3.client(
+            "glue",
+            region_name=API_REGION,
+            aws_access_key_id=API_KEY,
+            aws_secret_access_key=API_SECRET
+        )
+
+        glue_client.batch_create_partition(
+            DatabaseName=database_name,
+            TableName=table_name,
+            PartitionInputList=[
+                {
+                    "Values": [file_date],
+                    "StorageDescriptor": {
+                        "Location": final_location,
+                        "InputFormat": "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat",
+                        "OutputFormat": "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat",
+                        "SerdeInfo": {
+                            "SerializationLibrary": "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
+                        },
+                    },
+                }
+            ],
+        )
+        logger.info(f"Partição {file_date} registrada no Glue")
+        return True
+    except ClientError as e:
         raise
 
 logger.info("Início")
@@ -64,9 +114,13 @@ s3_client = boto3.client(
     aws_access_key_id=API_KEY,
     aws_secret_access_key=API_SECRET
 )
-lista = list_local_dates(DEFAULT_LOCAL_DIR)
-print(lista)
-upload = list_uploaded_dates(s3_client, BUCKET_NAME, RAW_LAYER_PREFIX)
-print(upload)
-dts = check_dates_to_upload(s3_client, BUCKET_NAME, RAW_LAYER_PREFIX, DEFAULT_LOCAL_DIR)
-print(dts)
+# resultado = glue_register_partition(ATHENA_DATABASE, TABLE_JOBS_LOGS, "2026-05-04", RAW_LAYER_LOCATION)
+# print(resultado)
+# result = s3_upload_file(s3_client, BUCKET_NAME, str(DEFAULT_LOCAL_DIR), RAW_LAYER_PREFIX, "2026-05-05")
+# print(result)
+# lista = list_local_dates(DEFAULT_LOCAL_DIR)
+# print(lista)
+# upload = list_uploaded_dates(s3_client, BUCKET_NAME, RAW_LAYER_PREFIX)
+# print(upload)
+# dts = check_dates_to_upload(s3_client, BUCKET_NAME, RAW_LAYER_PREFIX, DEFAULT_LOCAL_DIR)
+# print(dts)
