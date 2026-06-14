aabf06edb63a18db4acaf5d20acde40dedb000df
RodrigoSilva1989 <dataforge.br@gmail.com>
2026-06-13 22:09:49 -0300

    feat: build intermediate models and schema

---
+++ dbt_food_cost_monitor/models/intermediate/int_cost_by_domain.sql
@@ -0,0 +1,39 @@
+with source as (
+    select * from {{ref('stg_job_logs')}}
+),
+agegate_data_by_domain as (
+    select
+        dt_execucao,
+        nm_dominio,
+        count(id_job) as nr_total_jobs
+        count(
+            case
+                when st_status = 'success' then id_job
+            end
+         ) as nr_sucesso_jobs,
+        sum(nr_estimativa_custo_usd) as vl_total_custo_usd,
+        avg(nr_estimativa_custo_usd) as vl_medio_por_job,
+        max(nr_estimativa_custo_usd) as vl_maior_custo,
+        sum(nr_duracao_minutos) as nr_total_duracao_minutos,
+        avg(nr_duracao_minutos) as nr_medio_duracao_minutos
+    from source
+    group by
+        dt_execucao,
+        nm_dominio
+),
+calculation_success_rate as (
+    select
+        dt_execucao,
+        nm_dominio,
+        nr_total_jobs
+        nr_sucesso_jobs,
+        vl_total_custo_usd,
+        vl_medio_por_job,
+        vl_maior_custo,
+        nr_total_duracao_minutos,
+        nr_medio_duracao_minutos,
+        nr_sucesso_jobs / nr_total_jobs as nr_taxa_sucesso
+    from agegate_data_by_domain
+)
+
+select * from calculation_success_rate
--- dbt_food_cost_monitor/models/staging/stg_job_logs.sql
+++ dbt_food_cost_monitor/models/staging/stg_job_logs.sql
@@ -1,8 +1,7 @@
 with source as (
     select * from {{source('raw', 'job_logs')}}
 ),
-
-transform as (
+change_cast_types as (
     select
         cast(job_id as {{dbt.type_string()}}) as id_job,
         cast(execution_date as date) as dt_execucao,
@@ -13,9 +12,39 @@ transform as (
         cast(estimated_cost_usd as {{dbt.type_float()}}) as nr_estimativa_custo_usd,
         cast(status as {{dbt.type_string()}}) as st_status,
         cast(cluster_type as {{dbt.type_string()}}) as tp_cluster,
-        cast(created_at as {{dbt.type_timestamp()}}) as dt_criacao,
-        week_of_year(cast(execution_date as date)) as nr_semana_ano
+        cast(created_at as {{dbt.type_timestamp()}}) as dt_criacao
     from source
+),
+filter_negative_cost as (
+    select
+        id_job,
+        dt_execucao,
+        nm_dominio,
+        nm_job,
+        nr_duracao_minutos,
+        nr_dbu_consumidos,
+        nr_estimativa_custo_usd,
+        st_status,
+        tp_cluster,
+        dt_criacao
+    from change_cast_types
+    where 1 = 1
+    and nr_estimativa_custo_usd > 0
+),
+create_new_columns as (
+    select
+        id_job,
+        dt_execucao,
+        nm_dominio,
+        nm_job,
+        nr_duracao_minutos,
+        nr_dbu_consumidos,
+        nr_estimativa_custo_usd,
+        st_status,
+        tp_cluster,
+        dt_criacao,
+        cast(week(dt_execucao) as int) as nr_semana
+    from filter_negative_cost
 )
 
-select * from transform
+select * from create_new_columns
